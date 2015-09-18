03b77954a00b172bc619358a4d93ec39f9e101b7
Devian <info@devian.tk>
2015-09-18 23:15:06 +0200

    add oboe, bassoon

--- packages/instruments/populate.js
+++ packages/instruments/populate.js
@@ -1,7 +1,7 @@
 Meteor.startup(function() {
 	if (Instruments.find().count() == 0 && InstrumentCategories.find().count() == 0) {
 		_.each({
-			'musical instrument': ['piano', 'violin', 'viola', 'cello', 'flute', 'recorder', 'guitar', 'bass guitar', 'percussion'],
+			'musical instrument': ['piano', 'violin', 'viola', 'cello', 'flute', 'recorder', 'oboe', 'bassoon', 'guitar', 'bass guitar', 'percussion'],
 			'voice': ['soprano voice', 'alto voice', 'tenor voice', 'bass voice']			
 		}, function(instruments, category) {
 			var instrumentIds = []
