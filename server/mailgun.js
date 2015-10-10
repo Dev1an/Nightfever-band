cfb90b73e74a1e0d2bcb3dbc698c1b2976efe6fc
Devian <info@devian.tk>
2015-10-10 22:59:33 +0200

    send verification email when adding an account

--- server/mailgun.js
+++ server/mailgun.js
@@ -4,5 +4,6 @@ Meteor.methods({
 	addMailAddress(address) {
 		if (this.userId === null) throw new Meteor.Error('Not logged in')
 		Accounts.addEmail(this.userId, address)
+		Accounts.sendVerificationEmail(this.userId, address)
 	}
 })
