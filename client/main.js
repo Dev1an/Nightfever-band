41f10e067d635ca70b962d4974b232f2a02a0a3e
Damiaan Dufaux <damiaan@dpro.be>
2016-12-14 16:22:13 +0100

    Remove unneeded container

--- client/main.js
+++ client/main.js
@@ -2,11 +2,10 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Router, Route, browserHistory } from 'react-router'
 
-
-import {Login} from '/imports/UI/Pages/login'
+import {ComboBox} from 'meteor/universe:accounts-ui'
 
 render((
     <Router history={browserHistory}>
-        <Route path="/" component={Login} />
+        <Route path="/" component={ComboBox} />
     </Router>
 ), document.body)
--- imports/UI/Pages/login.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import {ComboBox} from 'meteor/universe:accounts-ui'
-
-export const Login = React.createClass({
-    render() {
-        return <ComboBox/>
-    }
-})
